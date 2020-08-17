963856d49323c0146d4c18d5f0b00d1f61233674
Rubén Carrascal <krrskl97@gmail.com>
2020-08-16 23:34:31 -0500

    :sparkles: General changes to the view of ordering an eagle and components.

--- src/app/core/models/direction.model.ts
+++ src/app/core/models/direction.model.ts
@@ -1,4 +1,5 @@
 export interface Direction {
+  id?: string;
   name: string;
   address: string;
   location: number[];
--- src/app/core/store/users-store/actions/user.actions.ts
+++ src/app/core/store/users-store/actions/user.actions.ts
@@ -8,6 +8,9 @@ const UserActionsTypes = {
   GET_FAVORITES_ADDRESS: '[USER] Get favorites address',
   GET_FAVORITES_ADDRESS_SUCCESS: '[USER] Get favorites address success',
 
+  SET_DIRECTION_ORIGIN: '[USER] Set origin direction',
+  SET_DIRECTION_DESTINATION: '[USER] Set destination direction',
+
   USER_FAILURE: '[USER] Failure',
 };
 
@@ -18,6 +21,16 @@ export const getFavoritesAddress = createAction(
 export const getFavoritesAddressSuccess = createAction(
   UserActionsTypes.GET_FAVORITES_ADDRESS_SUCCESS,
   props<{ favorites: Direction[] }>(),
+);
+
+export const setDirectionOrigin = createAction(
+  UserActionsTypes.SET_DIRECTION_ORIGIN,
+  props<{ origin: Direction }>(),
+);
+
+export const setDirectionDestination = createAction(
+  UserActionsTypes.SET_DIRECTION_DESTINATION,
+  props<{ destination: Direction }>(),
 );
 
 export const userFailure = createAction(
